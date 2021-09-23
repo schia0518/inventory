449357d0c7da4a4bba6f88b85f691f595ea9e8be
schia0518 <89336505+schia0518@users.noreply.github.com>
2021-09-23 14:20:00 +0100

    Update inventory_query.sql

--- inventory_query.sql
+++ inventory_query.sql
@@ -8,7 +8,7 @@ from inventory_fact;
 select InventoryKey, ItemMasterKey, DateKey,Quantity,
 sum(Quantity) over (partition by ItemMasterKey
 				    order by DateKey
-                    rows between current row and 3 following
+                    rows between current row and 8 following
                     ) as running_total
 from inventory_fact;
 
