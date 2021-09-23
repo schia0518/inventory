014095c08154463ee97d4e899eab1fbc83700ff8
schia0518 <89336505+schia0518@users.noreply.github.com>
2021-09-23 11:56:37 +0100

    Update inventory_query.sql

--- inventory_query.sql
+++ inventory_query.sql
@@ -1,13 +1,13 @@
 select InventoryKey, ItemMasterKey, DateKey,Quantity,
 sum(Quantity) over (partition by ItemMasterKey
-				     order by DateKey
+				     order by InventoryKey
                     range between 5 preceding and current row
                     ) as running_total
 from inventory_fact;
 
 select InventoryKey, ItemMasterKey, DateKey,Quantity,
 sum(Quantity) over (partition by ItemMasterKey
-				    order by DateKey
+				    order by InventoryKey
                     rows between current row and 3 following
                     ) as running_total
 from inventory_fact;
